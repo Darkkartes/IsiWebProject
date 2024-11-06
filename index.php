177df2919874d63122c65eeee6a8ab1ed8630d14
fldup <lilas.grenier@etu.univ-lyon1.fr>
2024-11-06 13:00:12 +0100

    architecture - nearly done

--- index.php
+++ index.php
@@ -2,7 +2,6 @@
 require_once('connect.php'); 
 require_once('model/model.php');
 
-$users  = get_users($db);
 require_once('close.php'); 
 require_once('vendor/autoload.php');
 $loader = new Twig\Loader\FilesystemLoader('view'); 
