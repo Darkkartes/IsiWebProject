c5e1e1c769f85914a8228a32836554255a1047e0
fldup <lilas.grenier@etu.univ-lyon1.fr>
2024-11-08 15:08:51 +0100

    all or nothing

---
+++ index.php
@@ -0,0 +1,1 @@
+require 'routes.php';
---
+++ indexBIS.php
@@ -0,0 +1,9 @@
+<?php 
+require_once('connect.php'); 
+require_once('model/model.php');
+
+require_once('close.php'); 
+require_once('vendor/autoload.php');
+$loader = new Twig\Loader\FilesystemLoader('view'); 
+$twig   = new Twig\Environment($loader); 
+echo $twig->render('index.twig');
