79c55c18fcfb0e2c5dcfd5e4c574827e2cf9b458
Luka COUTANT <luka.coutant@etu.univ-lyon1.fr>
2025-01-02 14:41:58 +0100

    affichage stagiaire

--- model/model.php
+++ model/model.php
@@ -9,69 +9,75 @@ function returnFalse()
     return false;
 }
 
-function get_entreprises($db){
-    $sql = 'SELECT * FROM `entreprise`'; 
-    $query = $db->prepare($sql); 
-    $query->execute(); 
+function get_entreprises($db)
+{
+    $sql = 'SELECT * FROM `entreprise`';
+    $query = $db->prepare($sql);
+    $query->execute();
     $result = $query->fetchAll(PDO::FETCH_ASSOC);
     return $result;
 }
-function get_entreprise_par_id($db, $id){
+function get_entreprise_par_id($db, $id)
+{
     $id = strip_tags($id);
-    $sql = 'SELECT * FROM `entreprise` where num_entreprise = :id'; 
-    $query = $db->prepare($sql); 
+    $sql = 'SELECT * FROM `entreprise` where num_entreprise = :id';
+    $query = $db->prepare($sql);
     $query->bindValue(':id', $id, PDO::PARAM_STR);
-    $query->execute(); 
+    $query->execute();
     $result = $query->fetchAll(PDO::FETCH_ASSOC);
     return $result;
 }
 
-function get_etudiants($db){
-    $sql = 'SELECT * FROM `etudiant`'; 
-    $query = $db->prepare($sql); 
-    $query->execute(); 
+function get_etudiants($db)
+{
+    $sql = 'SELECT * FROM `etudiant`';
+    $query = $db->prepare($sql);
+    $query->execute();
     $result = $query->fetchAll(PDO::FETCH_ASSOC);
     log($result);
     return $result;
 }
 
-function get_etudiant_par_id($db, $id){
+function get_etudiant_par_id($db, $id)
+{
     $id = strip_tags($id);
-    $sql = 'SELECT * FROM `etudiant` where num_etudiant = :id'; 
-    $query = $db->prepare($sql); 
+    $sql = 'SELECT * FROM `etudiant` where num_etudiant = :id';
+    $query = $db->prepare($sql);
     $query->bindValue(':id', $id, PDO::PARAM_STR);
-    $query->execute(); 
+    $query->execute();
     $result = $query->fetchAll(PDO::FETCH_ASSOC);
     return $result;
 }
 
-function get_stagiaires($db){
+function get_stagiaires($db)
+{
     $sql = 'SELECT nom_etudiant, prenom_etudiant, raison_sociale, nom_prof, prenom_prof 
             FROM `stage` 
-            join etudiant using (num_etudiant) 
-            join professeur using (num_prof) 
-            join entreprise using (num_entreprise); '; 
-    $query = $db->prepare($sql); 
-    $query->execute(); 
-    $result = $query->fetchAll(PDO::FETCH_ASSOC);
-    log($result);
-    return $result;
-}
-
-function get_stages($db){
-    $sql = 'SELECT * FROM `stage`'; 
-    $query = $db->prepare($sql); 
-    $query->execute(); 
+            join `etudiant` using (num_etudiant) 
+            join `professeur` using (num_prof) 
+            join `entreprise` using (num_entreprise);';
+    $query = $db->prepare($sql);
+    $query->execute();
     $result = $query->fetchAll(PDO::FETCH_ASSOC);
     return $result;
 }
 
-function get_stage_par_id($db, $id){
+function get_stages($db)
+{
+    $sql = 'SELECT * FROM `stage`';
+    $query = $db->prepare($sql);
+    $query->execute();
+    $result = $query->fetchAll(PDO::FETCH_ASSOC);
+    return $result;
+}
+
+function get_stage_par_id($db, $id)
+{
     $id = strip_tags($id);
-    $sql = 'SELECT * FROM `stage` where num_stage = :id'; 
-    $query = $db->prepare($sql); 
+    $sql = 'SELECT * FROM `stage` where num_stage = :id';
+    $query = $db->prepare($sql);
     $query->bindValue(':id', $id, PDO::PARAM_STR);
-    $query->execute(); 
+    $query->execute();
     $result = $query->fetchAll(PDO::FETCH_ASSOC);
     return $result;
 }
